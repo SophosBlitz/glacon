105fe8856c32a0eaea261967252b3dfb31da9ac0
Sophos Blitz <SophosBlitz@users.noreply.github.com>
2014-06-05 14:09:36 -0700

    Added the scp plugin.

---
+++ plugins/scp.py
@@ -0,0 +1,125 @@
+"""
+scp.py: Written by Gnosis for Grapewhistle 2009, inherited by Glacon in 2011
+v1.0 of basic in-channel data acquisition plugins for IRC channels of the SCP Foundation.
+"""
+from util import hook, http
+import threading
+from time import sleep
+import sqlite3
+import re
+import os
+
+class SCPThread(threading.Thread):
+    def __init__(self, dbpath):
+        threading.Thread.__init__(self, name="SCP")
+        self.dbpath = dbpath
+    def run(self):
+        db = sqlite3.connect(self.dbpath)
+        db.execute("create table if not exists scps(number varchar primary key, title varchar)")
+        db.text_factory = str
+
+	basescpurl = "http://www.scp-wiki.net"
+        scpseriespages = ["/scp-series", "/scp-series-2", "/scp-series-3", "/scp-ex"]
+
+        scpinterval = 60 * 5
+        scpcounter = 0
+
+	# Infinite query loop
+        while True:
+            try:
+                c = db.cursor()
+                c.execute("delete from scps")
+                scpcounter = 0
+
+		# Grab main list entries from each page
+                for scpseriespage in scpseriespages:
+                    page = http.to_utf8(http.get(basescpurl + scpseriespage))
+                    scp_re = re.compile(r'<a href="/scp-(.*)">SCP-\1</a> - (.*?)</li>', re.I)
+                    scp_list = scp_re.findall(page)
+
+                    # Add entries to database
+                    for (k, v) in scp_list:
+                        #print k, v
+                        c.execute(u"replace into scps(number, title) values (upper(?), ?)", (k, v))
+                        scpcounter = scpcounter + 1
+                    db.commit()
+                    #print "scp.py - Updated SCP database from listing on", scpseriespage
+                c.close()
+                print "scp.py - SCP database update complete, %d total entries." % scpcounter
+            except Exception, e:
+                    print "ERROR ERROR ERROR, ", e
+            # Query every 5 minutes
+            sleep(scpinterval)
+
+# Looks up SCP from cached database. Returns [ACCESS DENIED] if not in database.
+def scp_lookup(number,  title=None):
+    url = "http://www.scp-wiki.net/scp-%s" % number
+    db = sqlite3.connect(scp_path)
+    if not title:
+        try: title = db.execute("select title from scps where number = ?", (number.upper(),)).fetchone()[0]
+        except TypeError: title = "[ACCESS DENIED]"
+    return "%s - %s" % (url, title)
+
+def scp_init(dbpath):
+    if all([thread.name != "SCP" for thread in threading.enumerate()]):
+        scp_thread = SCPThread(scp_path)
+        scp_thread.start()
+        sleep(1)
+
+@hook.regex(r'^SCP-((?:\d|-|J)+)$', re.I)
+def scp(inp, bot=None, input=None):
+    try: inp = inp.groups()[0]
+    except AttributeError: pass
+
+    dbpath = os.path.join(bot.persist_dir, "%s.%s.db" % (input.conn.nick, input.conn.server))
+
+    return scp_lookup(inp)
+
+# Query multiple SCPs at once with a comma-delimited list of !scp-xxxx tokens
+@hook.event('PRIVMSG')
+def multiscp(inp, bot=None, input=None):
+    scps = re.compile('!SCP-((?:\d|-|J)+)', re.I).findall(inp[1])
+    for scp in scps:
+        input.reply(scp_lookup(scp))
+
+# Fetches the rating from the HTML page of the target SCP.
+@hook.command
+def rating(inp):
+    if "scp-" not in inp: inp = "scp-" + inp
+    print ("Calling http.get() on http://www.scp-wiki.net/%s" % inp)
+    page = http.get("http://www.scp-wiki.net/%s" % inp)
+    rating = http.get_html("http://www.scp-wiki.net/%s" % inp).xpath("//*[@class='number prw54353']/text()")[0]
+    return rating
+
+# Searches cached database for keyword in SCP name/title.
+@hook.command
+def search(inp):
+    inp = "%" + inp + "%"
+    db = sqlite3.connect(scp_path)
+    scps =  db.execute("SELECT NUMBER, TITLE FROM SCPS WHERE TITLE LIKE ?", (inp,)).fetchall()
+    if len(scps) == 1:
+        (number, title) = scps[0]
+        return "SCP-%s - %s - http://www.scp-wiki.net/scp-%s" % (number, title, number)
+    printed = scps[:5]
+    output = ""
+    for (number, title) in printed:
+        output += "SCP-%s (%s), " % (number, title)
+    if len(scps) > 5: output += " plus %d more" % (len(scps) - 5)
+    else: output = output[:-2]
+    if not output: return "No SCPs found."
+    return output
+
+# Returns a random SCP in cached database.
+@hook.command
+def random(inp):
+    db = sqlite3.connect(scp_path)
+    (number, title)  = db.execute("SELECT NUMBER, TITLE FROM SCPS ORDER BY RANDOM() LIMIT 1").fetchone()
+    return "SCP-%s - %s - http://www.scp-wiki.net/scp-%s" % (number, title, number)
+
+# Just returns the tag page for now.
+def tag(inp):
+    return "http://www.scp-wiki.net/system:page-tags/tag/" + inp
+
+mydir = dir()
+scp_path = os.path.join(os.path.abspath('persist'), "scp.db")
+scp_init(scp_path)
